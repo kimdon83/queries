8e219454dc153411e53eac8fe2ec28634830087d
kimdon83 <donghun.kim.9003@gmail.com>
2022-11-17 16:33:26 -0500

    add BI data to SQL.sql

---
+++ BI data to SQL server check.sql
@@ -0,0 +1,18 @@
+SELECT top(10) * FROM [ivy.sd.fact.order]
+ORDER BY act_date desc
+
+SELECT top(10) * FROM [ivy.sd.fact.ar]
+
+SELECT top(10) * FROM [ivy.sd.fact.visit]
+ORDER BY act_date desc
+
+-------
+SELECT top(10) * FROM [ivy.sd.fact.return]
+ORDER BY act_date desc
+
+SELECT top(10) * FROM [ivy.sd.fact.bo]
+ORDER BY act_date desc
+
+SELECT top(10) * FROM [ivy.sd.fact.bill]
+ORDER BY act_date desc
+-----
